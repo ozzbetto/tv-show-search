b3fdf12f7a7335a103f5d1be3d8196ff4b5a6898
ozzbetto <segovia.al@gmail.com>
2021-08-05 21:50:34 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('#searchForm');
+const query = document.querySelector('#query');
+
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    resetImages();
+    const searchTerm =  form.elements.query.value;
+    const config = { params: {q: searchTerm} };
+    const res = await axios.get(` https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data);
+    form.elements.query.value = '';
+})
+
+const makeImages = (shows) => {
+    for(let result of shows) {
+        if(result.show.image) {
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
+
+resetImages = () => {
+    const images = document.querySelectorAll('img');
+
+    for(let eachImage of images) {
+        eachImage.remove();
+    }
+}
